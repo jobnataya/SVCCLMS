6078c0e65da740bf0e5ce35335edf193c4d01b96
Job Nataya <jobpaznataya@gmail.com>
2023-05-13 00:42:23 +0800

    create with login na

--- index.php
+++ index.php
@@ -7,6 +7,40 @@
     <title>Login</title>
     <link rel="stylesheet" href="CSS/indexstyle.css">
 </head>
+<?php
+session_start();
+include("connect.php");
+
+if($_SERVER['REQUEST_METHOD'] == 'POST')
+{
+    $fname = $_POST['fname'];
+    $password = $_POST['Pword'];
+
+    if (!empty($fname) && !empty($password)) 
+    {
+        $query = "SELECT * FROM svccinfo WHERE name = '$fname' LIMIT 1 ";
+        $result = mysqli_query($conn, $query);
+
+        if($result)
+        {
+            if($result && mysqli_num_rows($result)> 0)
+            {
+                $user_data = mysqli_fetch_assoc($result);
+
+                if ($user_data['password'] == $password) 
+                {
+                    header("Location: home.php");
+                    die;
+                }
+            }
+        }
+        echo "<script type = 'text/javascript'> alert ('Wrong Username!')</script>";
+    }else{
+        echo "<script type = 'text/javascript'> alert ('Wrong Password!')</script>";
+    }
+}
+
+?>
 <body>
     <div class="header">
         <div class="Logo">
@@ -21,10 +55,10 @@
         <div class="login">
             <form  method="POST">
                 <label for="Name">Name:</label>
-                <input class="name" type="text" id="name" placeholder="User Name:">
+                <input class="name" type="text" id="name" placeholder="User Name:" name="fname" >
                 <br>
                 <label for="Password">Password</label>
-                <input class="password" type="password" id="password" placeholder="Password:">
+                <input class="password" type="password" id="password" placeholder="Password:" name="Pword" >
                 <br>
                 <br>
                 <input class="slogin" type="submit" id="submit" value="Log in">
